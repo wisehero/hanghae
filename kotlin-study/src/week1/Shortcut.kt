35fd3b7414e4a37dd2556d63345dba222db11e5b
wisehero <godjw96@gmail.com>
2025-01-27 01:27:35 +0900

    코틀린 1주차 - 단축키

---
+++ kotlin-study/src/week1/Shortcut.kt
@@ -0,0 +1,19 @@
+package week1
+
+fun main() {
+    println("hello World!");
+
+    // Command + Delete = 한 줄 삭제
+    println("이걸 지워보기")
+
+    // 편집기 영역이 아닌 다른 곳을 클릭한 후 ESC를 누르면 편집기 영역으로 이동
+    // Command + Shif + F = 전체 찾기
+    // Command + S = 저장
+    // Command + C, Command + V 복사, 붙여넣기
+    // Shift + 방향키<상,하,좌,우> = 라인 드래그
+
+    // 과제 영역
+    // Command + A = 파일 전체 드래그
+    // 라인을 전체 드래그 한후 Shift + fn + 방향키 = 라인 위로 전체 드래그 혹은 라인 아래로 전체 드래그
+
+}
