9be1d3be7d769acb686b946adc129b03c2b790be
wisehero <godjw96@gmail.com>
2025-01-27 01:28:02 +0900

    코틀린 2주차 - 변수

---
+++ kotlin-study/src/week2/Variables.kt
@@ -0,0 +1,23 @@
+package week2
+
+fun main() {
+    // 예시 1: 정수형 변수
+    val myNumber: Int = 10
+    println("정수형 변수: $myNumber")
+
+    // 예시 2: 부동소수점 변수
+    val myFloat: Float = 3.14f
+    println("부동소수점 변수: $myFloat")
+
+    // 예시 3: 문자형 변수
+    val myChar: Char = 'A'
+    println("문자형 변수: $myChar")
+
+    // 예시 4: 문자열 변수
+    val myString: String = "Hello, Kotlin!"
+    println("문자열 변수: $myString")
+
+    // 예시 5: 논리형 변수
+    val myBoolean: Boolean = true
+    println("논리형 변수: $myBoolean")
+}
