c0b060ad769a0fc75f849aa078c1af024727709c
wisehero <godjw96@gmail.com>
2025-01-27 01:28:19 +0900

    코틀린 2주차 - 상수

---
+++ kotlin-study/src/week2/Constant.kt
@@ -0,0 +1,9 @@
+package week2
+
+fun main() {
+    val name = "참새"
+    println("새의 이름을 지정하는 name 변수에는 ${name}이 들어있어")
+
+//    name = "조롱박이" val로 선언한 변수는 값을 변경할 수 없다.
+    println("새의 이름을 변경하려고 하지만 ${name}이라는 이름은 바꿀 수 없어")
+}
