4e4c0b0bb69db5cceee0e50720052fa403adc77c
wisehero <godjw96@gmail.com>
2025-01-30 00:48:39 +0900

    Chap03 - 리팩토링(변수를 상수로)

--- tdd-kotlin/src/test/java/chap03/ExpiryDateCalculator.java
+++ tdd-kotlin/src/test/java/chap03/ExpiryDateCalculator.java
@@ -4,18 +4,20 @@ import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
 
+	int addedMonths = 1;
+
 	public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
 		return billingDate.plusMonths(1);
 	}
 
 	public LocalDate calculateExpiryDate(PayData payData) {
 		if (payData.getFirstBillingDate() != null) {
-			LocalDate candidateExp = payData.getBillingDate().plusMonths(1);
+			LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
 			if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
 				return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
 			}
 		}
 
-		return payData.getBillingDate().plusMonths(1);
+		return payData.getBillingDate().plusMonths(addedMonths);
 	}
 }
