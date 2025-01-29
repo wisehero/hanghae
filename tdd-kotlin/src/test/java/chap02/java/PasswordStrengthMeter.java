06c2a3d9b2ea7b0c97a1d470959d3b0449a13681
wisehero <godjw96@gmail.com>
2025-01-29 20:25:45 +0900

    자바 계산기 테스트 - 리팩토링 (숫자가 존재하는지 검증)

--- tdd-kotlin/src/test/java/chap02/java/PasswordStrengthMeter.java
+++ tdd-kotlin/src/test/java/chap02/java/PasswordStrengthMeter.java
@@ -1,10 +1,25 @@
 package chap02.java;
 
 public class PasswordStrengthMeter {
+
 	public PasswordStrength meter(String s) {
 		if (s.length() < 8) {
 			return PasswordStrength.NORMAL;
 		}
+		boolean containsNum = meetsContainingNumberCriteria(s);
+		if (!containsNum) {
+			return PasswordStrength.NORMAL;
+		}
 		return PasswordStrength.STRONG;
+	}
+
+	// 0~9 사이의 숫자를 포함하는지 검사하는 메서드
+	private boolean meetsContainingNumberCriteria(String s) {
+		for (char ch : s.toCharArray()) {
+			if (ch >= '0' && ch <= '9') {
+				return true;
+			}
+		}
+		return false;
 	}
 }
