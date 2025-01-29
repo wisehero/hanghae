82b74c034ce67df7e421d2f3763cd9ca74240b88
wisehero <godjw96@gmail.com>
2025-01-30 00:56:55 +0900

    Chap03 - 새로운 케이스가 통과되도록 로직 수정

--- tdd-kotlin/src/test/java/chap03/ExpiryDateCalculator.java
+++ tdd-kotlin/src/test/java/chap03/ExpiryDateCalculator.java
@@ -4,13 +4,12 @@ import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
 
-	int addedMonths = 1;
-
 	public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
 		return billingDate.plusMonths(1);
 	}
 
 	public LocalDate calculateExpiryDate(PayData payData) {
+		int addedMonths = payData.getPayAmount() / 10_000;
 		if (payData.getFirstBillingDate() != null) {
 			LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
 			if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
