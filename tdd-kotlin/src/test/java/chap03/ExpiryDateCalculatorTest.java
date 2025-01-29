0e43c23a72a450d9bd23d7b8fb13018fd2462fc5
wisehero <godjw96@gmail.com>
2025-01-30 01:19:32 +0900

    Chap03 - 로직 수정 후 더 많은 케이스 추가해서 테스트

--- tdd-kotlin/src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ tdd-kotlin/src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -110,6 +110,22 @@ public class ExpiryDateCalculatorTest {
 				.payAmount(20_000)
 				.build(),
 			LocalDate.of(2025, 4, 30));
+
+		assertExpiryDate(
+			PayData.builder()
+				.firstBillingDate(LocalDate.of(2025, 1, 31))
+				.billingDate(LocalDate.of(2025, 2, 28))
+				.payAmount(40_000)
+				.build(),
+			LocalDate.of(2025, 6, 30));
+
+		assertExpiryDate(
+			PayData.builder()
+				.firstBillingDate(LocalDate.of(2025, 3, 31))
+				.billingDate(LocalDate.of(2025, 4, 30))
+				.payAmount(30_000)
+				.build(),
+			LocalDate.of(2025, 7, 31));
 	}
 
 	private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
