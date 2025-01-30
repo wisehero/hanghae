91554278fd64275ff5f1e99b740658a51534c62f
wisehero <godjw96@gmail.com>
2025-01-30 21:40:08 +0900

    Chap07 - 회원가입 후 이메일 발송 기능을 테스트하기 위한 스파이 객체 작성

--- tdd-kotlin/src/test/java/chap07/user/UserRegisterTest.java
+++ tdd-kotlin/src/test/java/chap07/user/UserRegisterTest.java
@@ -2,7 +2,6 @@ package chap07.user;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,10 +12,11 @@ public class UserRegisterTest {
 	private UserRegister userRegister;
 	private StubWeakPasswordChecker stubWeakPasswordChecker = new StubWeakPasswordChecker();
 	private MemoryUserRepository fakeRepository = new MemoryUserRepository();
+	private SpyEmailNotifier spyEmailNotifier = new SpyEmailNotifier();
 
 	@BeforeEach
 	void setUp() {
-		userRegister = new UserRegister(stubWeakPasswordChecker, fakeRepository);
+		userRegister = new UserRegister(stubWeakPasswordChecker, fakeRepository, spyEmailNotifier);
 	}
 
 	@Test
@@ -47,5 +47,13 @@ public class UserRegisterTest {
 		User savedUser = fakeRepository.findById("id"); // 가입 결과 확인
 		assertThat(savedUser.getId()).isEqualTo("id");
 		assertThat(savedUser.getEmail()).isEqualTo("email");
+	}
+
+	@Test
+	@DisplayName("가입하면 메일을 전송함")
+	void whenRegisterThenSendMail() {
+		userRegister.register("id", "pw", "email");
+		assertThat(spyEmailNotifier.isCalled()).isTrue();
+		assertThat(spyEmailNotifier.getEmail()).isEqualTo("email");
 	}
 }
