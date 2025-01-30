29b0a57e7191e7f433e48555f621ca2c8a342fa5
wisehero <godjw96@gmail.com>
2025-01-30 19:41:35 +0900

    Chap07 - 컴파일 에러를 없애기 위한 최소한의 기능 구현

---
+++ tdd-kotlin/src/test/java/chap07/user/StubWeakPasswordChecker.java
@@ -0,0 +1,10 @@
+package chap07.user;
+
+public class StubWeakPasswordChecker implements WeakPasswordChecker {
+
+	private boolean weak;
+
+	public void setWeak(boolean weak) {
+		this.weak = weak;
+	}
+}
---
+++ tdd-kotlin/src/test/java/chap07/user/UserRegister.java
@@ -0,0 +1,13 @@
+package chap07.user;
+
+public class UserRegister {
+	private WeakPasswordChecker passwordChecker;
+
+	public UserRegister(WeakPasswordChecker passwordChecker) {
+		this.passwordChecker = passwordChecker;
+	}
+
+	public void register(String id, String pw, String email) {
+
+	}
+}
