342ccda14d6f11cee7ff8d3992330c0c367b5892
wisehero <godjw96@gmail.com>
2025-01-30 19:41:17 +0900

    Chap07 - 암호 강도 테스트 작성

---
+++ tdd-kotlin/src/test/java/chap07/user/UserRegisterTest.java
@@ -0,0 +1,26 @@
+package chap07.user;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserRegisterTest {
+	private UserRegister userRegister;
+	private StubWeakPasswordChecker stubWeakPasswordChecker = new StubWeakPasswordChecker();
+
+	@BeforeEach
+	void setUp() {
+		userRegister = new UserRegister(stubWeakPasswordChecker);
+	}
+
+	@Test
+	@DisplayName("약한 암호면 가입 실패")
+	void weakPassword() {
+		stubWeakPasswordChecker.setWeak(true); // 약한 암호로 설정
+
+		Assertions.assertThatThrownBy(() -> {
+			userRegister.register("id", "pw", "email");
+		}).isInstanceOf(WeakPasswordException.class);
+	}
+}
