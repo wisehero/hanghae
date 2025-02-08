506b3d7551a547828c0d5b45a0ca8490409382ba
wisehero <godjw96@gmail.com>
2025-02-08 12:37:43 +0900

    val로 선언한 프로퍼티는 읽기 전용이며 getter가 제공되고 var로 선언한 필드는 getter setter가 모두 제공된다.

--- kotlin-study/src/kotlin_in_action/chap02/KotlinPerson.kt
+++ kotlin-study/src/kotlin_in_action/chap02/KotlinPerson.kt
@@ -1,4 +1,20 @@
 package kotlin_in_action.chap02
 
 // 코틀린의 기본 가시성은 public이므로 자바와는 다르게 생략이 가능하다.
-class Person(val name: String)
+// val로 선언한 키워드는 읽기 전용이며 getter만 제공된다.
+// 반면 var로 선언한 키워드는 setter와 getter가 모두 제공된다.
+
+fun main() {
+    val person = Person("Bob", true)
+    println(person.name)
+    println(person.isMarriged)
+
+    person.isMarriged = false
+    println(person.isMarriged)
+}
+
+class Person(
+    val name: String,
+    var isMarriged: Boolean
+) {
+}
