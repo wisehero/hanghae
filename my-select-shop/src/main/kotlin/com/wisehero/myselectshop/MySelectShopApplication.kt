f0208b57e861e6100e445dd283892808bf759600
김지웅 <iop1996@easyshop.co.kr>
2025-02-10 17:07:29 +0900

    My-Select-Shop : 프로젝트 셋팅

---
+++ my-select-shop/src/main/kotlin/com/wisehero/myselectshop/MySelectShopApplication.kt
@@ -0,0 +1,11 @@
+package com.wisehero.myselectshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MySelectShopApplication
+
+fun main(args: Array<String>) {
+	runApplication<MySelectShopApplication>(*args)
+}
---
+++ my-select-shop/src/test/kotlin/com/wisehero/myselectshop/MySelectShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wisehero.myselectshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MySelectShopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
