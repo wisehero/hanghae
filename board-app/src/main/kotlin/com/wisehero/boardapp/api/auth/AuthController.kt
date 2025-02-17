35310e60ae92417441244d050be9f26ebeb1ab29
김지웅 <iop1996@easyshop.co.kr>
2025-02-17 10:25:24 +0900

    코틀린 게시판 - AuthService 추가

--- board-app/src/main/kotlin/com/wisehero/boardapp/api/auth/AuthController.kt
+++ board-app/src/main/kotlin/com/wisehero/boardapp/api/auth/AuthController.kt
@@ -2,10 +2,7 @@ package com.wisehero.boardapp.api.auth
 
 import com.wisehero.boardapp.api.auth.request.LoginRequest
 import com.wisehero.boardapp.api.auth.request.LoginUserInfo
-import com.wisehero.boardapp.domain.auth.LoginToken
-import com.wisehero.boardapp.domain.auth.LoginUser
-import com.wisehero.boardapp.domain.auth.RefreshTokenRepository
-import com.wisehero.boardapp.domain.auth.TokenProvider
+import com.wisehero.boardapp.domain.auth.*
 import com.wisehero.boardapp.domain.user.UserRepository
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.web.bind.annotation.GetMapping
@@ -18,22 +15,13 @@ import java.util.*
 @RestController
 @RequestMapping("/api/auth")
 class AuthController(
-    private val userRepository: UserRepository,
-    private val bCryptPasswordEncoder: BCryptPasswordEncoder,
-    private val refreshTokenRepository: RefreshTokenRepository,
-    private val tokenProvider: TokenProvider
+    private val authService: AuthService,
+    private val bCryptPasswordEncoder: BCryptPasswordEncoder
 ) {
 
     @PostMapping("/login")
     fun login(@RequestBody request: LoginRequest): LoginToken {
-        val user = userRepository.findByEmail(request.email) ?: throw IllegalArgumentException("존재하지 않는 계정입니다.")
-        require(bCryptPasswordEncoder.matches(request.password, user.password)) {
-            throw RuntimeException("비밀번호가 일치하지 않습니다.")
-        }
-
-        if (refreshTokenRepository.existsByUserId(user.id!!)) {
-            refreshTokenRepository.deleteByUserId(user.id!!)
-        }
-        return tokenProvider.getToken(user.id!!, user.role.name, Date())
+        val encodedPassword = bCryptPasswordEncoder.encode(request.password)
+        return authService.authenticate(request.email, encodedPassword)
     }
 }
---
+++ board-app/src/main/kotlin/com/wisehero/boardapp/domain/auth/AuthService.kt
@@ -0,0 +1,29 @@
+package com.wisehero.boardapp.domain.auth
+
+import com.wisehero.boardapp.domain.user.UserRepository
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+@Service
+class AuthService(
+    private val userRepository: UserRepository,
+    private val refreshTokenRepository: RefreshTokenRepository,
+
+    private val tokenProvider: TokenProvider
+) {
+
+    @Transactional
+    fun authenticate(email: String, encryptedPassword: String): LoginToken {
+        val user = userRepository.findByEmail(email) ?: throw IllegalArgumentException("존재하지 않는 계정입니다.")
+        require(user.password != encryptedPassword) {
+            throw RuntimeException("비밀번호가 일치하지 않습니다.")
+        }
+
+        if (refreshTokenRepository.existsByUserId(user.id!!)) {
+            refreshTokenRepository.deleteByUserId(user.id!!)
+        }
+        return tokenProvider.getToken(user.id!!, user.role.name, Date())
+    }
+}
